885412c5581984217d133f1bf28ae0a7ba0c21d6
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-01 19:41:47 -0500

    try simulate each 'instruction'

--- day1part2.py
+++ day1part2.py
@@ -1,17 +1,18 @@
-def dial(rotations):
+def dial(inputs):
     dial = 50
     password = 0
-    for rotation in rotations:
-        steps = int(rotation[1:])
-        if rotation[0] == "R":
-            password += (dial + steps) // 100
-            dial = (dial + steps) % 100
-        elif rotation[0] == "L":
-            steps = steps % 100
-            if steps > dial:
-                password += 1 + (steps - dial - 1) // 100
-            dial = (dial - steps) % 100
-
+    for instruction in inputs:
+        steps = int(instruction[1:])
+        if instruction[0] == "R":
+            for _ in range(steps):
+                dial = (dial + 1) % 100
+                if dial == 0:
+                    password += 1
+        elif instruction[0] == "L":
+            for _ in range(steps):
+                dial = (dial - 1) % 100
+                if dial == 0:
+                    password += 1
     return password
 
 def main():
