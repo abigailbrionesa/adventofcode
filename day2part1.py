c14df4af8145bf3a41d1aeeec17d01e50d69887f
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-02 05:50:37 -0500

    sum not count

--- day2part1.py
+++ day2part1.py
@@ -10,28 +10,29 @@ def is_invalid(input):
     else:
         left  = str_input[:mid]
         right = str_input[mid:]
+    right = right.lstrip('0')
     if not left or not right:
         return False
     if left[0] == '0' or right[0] == '0':
         return False
     return left == right
 
-def invalid_count(input):
-    count = 0
+def invalid_sum(input):
+    total = 0
     for id in input:
-        if is_invalid(id):
-            count +=1
-    return count
+        if invalid_sum(id):
+            total += id
+    return total
     
 def main():
-    with open("input2.txt") as f:
+    with open("input2example.txt") as f:
         line = f.read().strip()
     ids = []
     for group in line.split(','):
         a,b = group.split('-')
         for ids_range in range(int(a), int(b)+1):
             ids.append(ids_range)
-    result = sum(is_invalid(id) for id in ids)
+    result = invalid_sum(ids)
     print("invalid count:", result)
 
 if __name__ == "__main__":
