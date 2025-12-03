41876bd740bfa80b2e8d64c141d2d46836928fa0
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-03 03:41:12 -0500

    read lines

--- day3part1.py
+++ day3part1.py
@@ -1,6 +1,6 @@
 # lobby
 def max_jolt(input):
-    s = str(input)
+    s = input.strip()
     n = len(s)
     max = float('-inf')
     last = 0
@@ -15,10 +15,27 @@ def max_jolt(input):
     max = float('-inf')
     for j in range(last+1,n):
         c = s[j]
-        print(c)
-        if int(j) > max:
+        if int(c) > max:
             max = int(c)
     res.append(str(max))
     return ''.join(res)
 
-print(max_jolt(234234234234278))
+def total_output(inputs):
+    total = 0
+    for row in inputs:
+        total += int(max_jolt(row))
+    return total
+
+def main():
+    with open("input3example.txt") as f:
+        lines = f.readlines()
+    total = total_output(lines)
+    print('result:', total)
+
+if __name__ == "__main__":
+    main()
+
+    
+    
+
+        
